74213b2813b4beda7d2480ba4d644b91c15077ed
Igryne <igryne.reategui@gmail.com>
2021-09-14 17:02:39 -0300

    Aula Prática 09 Curso em Video Java POO

---
+++ InterfaceLivro/src/br/com/exercicioaula09/Livro.java
@@ -0,0 +1,105 @@
+package br.com.exercicioaula09;
+
+public class Livro implements Publicacao{
+    private String titulo, autor;
+    private int totPaginas, pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    protected Livro() {
+    }
+
+
+    public String detalhes() {
+        return "Livro{" +
+                "\n titulo='" + titulo + '\'' +
+                "\n, autor='" + autor + '\'' +
+                "\n, totPaginas=" + totPaginas +
+                "\n, pagAtual=" + pagAtual +
+                "\n, aberto=" + aberto +
+                "\n, leitor=" + leitor +
+                '}';
+    }
+
+    public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        this.pagAtual = 0;
+        this.aberto = false;
+        this.leitor = leitor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean getAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    @Override
+    public void abrir() {
+        this.aberto = true;
+    }
+
+    @Override
+    public void fechar() {
+        this.aberto = false;
+    }
+
+    @Override
+    public void folhear(int p) {
+        this.pagAtual = p;
+    }
+
+    @Override
+    public void avancarPag() {
+        this.pagAtual++;
+    }
+
+    @Override
+    public void voltarPag() {
+        this.pagAtual--;
+    }
+}
---
+++ InterfaceLivro/src/br/com/exercicioaula09/ProjetoLivros.java
@@ -0,0 +1,18 @@
+package br.com.exercicioaula09;
+
+public class ProjetoLivros {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+
+        p[0] = new Pessoa("Pedro", 22, "M");
+        p[1] = new Pessoa("Maria", 25, "F");
+
+        l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+        l[1] = new Livro("A arte de ligar o foda-se", "Fulano", 200, p[1]);
+        l[2] = new Livro("Testando", "Fulana", 500, p[0]);
+
+        System.out.println(l[1].detalhes());
+
+    }
+}
